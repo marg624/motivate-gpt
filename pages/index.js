72ba6fa5dbdd52126ad5d565d52b2118f2004690
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-04-25 17:54:59 -0700

    add metadata

--- pages/index.js
+++ pages/index.js
@@ -123,6 +123,42 @@ export default function Home() {
 
 
   return (<>
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#000" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta
+        name="description"
+        content={`A collection of games to play with friends.`}
+      />
+      <meta property="og:image" content="https://wordle-with-friends.s3.us-west-2.amazonaws.com/motivate.png" />
+      </Head>
+
     <div className="min-h-screen">
         <div className="border-b bg-neutral-50 border-neutral-200">
           <div className="container mx-auto px-5">
